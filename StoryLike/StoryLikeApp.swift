93712079fa6ad0b3e50a09feabb58a5ec7017777
Nicolas VERGOZ <nicolas.vergoz@gmail.com>
2025-10-17 10:40:59 +0200

    Initial Commit

---
+++ StoryLike/StoryLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryLikeApp.swift
+//  StoryLike
+//
+//  Created by Nicolas Vergoz on 17/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoryLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
